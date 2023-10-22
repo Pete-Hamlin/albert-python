5e4afe18c1730dcfc824dd914e1bb982bc921609
pete <work@pete-hamlin.com>
2023-10-22 20:49:04 +0100

    feat: Added mullvad extension

---
+++ mullvad/__init__.py
@@ -0,0 +1,157 @@
+import re
+import subprocess
+from collections import namedtuple
+from pathlib import Path
+
+from albert import *
+
+md_iid = "2.1"
+md_version = "2.0"
+md_name = "Mullvad"
+md_description = "Manage mullvad VPN connections"
+md_license = "GPLv3"
+md_url = "https://github.com/albertlauncher/python"
+md_maintainers = ["@Pete-Hamlin"]
+md_credits = ["@janeklb", "@Bierchermuesli"]
+md_bin_dependencies = ["mullvad"]
+
+
+class Plugin(PluginInstance, TriggerQueryHandler):
+    VPNConnection = namedtuple("VPNConnection", ["name", "connected"])
+    iconUrls = ["xdg:network-wired"]
+    blockedIcon = ["file:{}".format(Path(__file__).parent / "lock-10.png")]
+    connectIcon = ["file:{}".format(Path(__file__).parent / "lock-9.png")]
+    disconnectIcon = ["file:{}".format(Path(__file__).parent / "lock-1.png")]
+
+    def __init__(self):
+        TriggerQueryHandler.__init__(
+            self,
+            id=md_id,
+            name=md_name,
+            description=md_description,
+            synopsis="<article-name>",
+            defaultTrigger="mullvad ",
+        )
+        PluginInstance.__init__(self, extensions=[self])
+
+        self.connection_regex = re.compile(r"[a-z]{2}-[a-z]*-[a-z]{2,4}-[\d]{2,3}")
+
+    def getRelays(self):
+        relayStr = subprocess.check_output("mullvad relay list", shell=True, encoding="UTF-8")
+        for relayStr in relayStr.splitlines():
+            relay = relayStr.split()
+            if relay and self.connection_regex.match(relay[0]):
+                yield (relay[0], relayStr)
+
+    def getIcon(self, status_string: str):
+        if "Blocked" in status_string:
+            return self.blockedIcon
+        if "Disconnected" in status_string:
+            return self.disconnectIcon
+        if "Connected" in status_string:
+            return self.connectIcon
+
+    def defaultItems(self):
+        statusStr = subprocess.check_output("mullvad status", shell=True, encoding="UTF-8").strip()
+        return [
+            StandardItem(
+                id="status",
+                text="Status",
+                subtext=statusStr,
+                iconUrls=self.getIcon(statusStr),
+                actions=[
+                    Action(
+                        "reconnect",
+                        "Reconnect",
+                        lambda: runDetachedProcess(["mullvad", "reconnect"]),
+                    ),
+                    Action(
+                        "connect",
+                        "Connect",
+                        lambda: runDetachedProcess(["mullvad", "connect"]),
+                    ),
+                    Action(
+                        "disconnect",
+                        "Disconnect",
+                        lambda: runDetachedProcess(["mullvad", "disconnect"]),
+                    ),
+                ],
+            ),
+        ]
+
+    def actions(self):
+        return [
+            StandardItem(
+                id="connect",
+                text="Connect",
+                subtext="Connect to default server",
+                iconUrls=self.connectIcon,
+                actions=[
+                    Action(
+                        "connect",
+                        "Connect",
+                        lambda: runDetachedProcess(["mullvad", "connect"]),
+                    )
+                ],
+            ),
+            StandardItem(
+                id="disconnect",
+                text="Disconnect",
+                subtext="Disconnect from VPN",
+                iconUrls=self.disconnectIcon,
+                actions=[
+                    Action(
+                        "disconnect",
+                        "Disconnect",
+                        lambda: runDetachedProcess(["mullvad", "disconnect"]),
+                    )
+                ],
+            ),
+            StandardItem(
+                id="reconnect",
+                text="Reconnect",
+                subtext="Reconnect to current server",
+                iconUrls=self.blockedIcon,
+                actions=[
+                    Action(
+                        "reconnect",
+                        "Reconnect",
+                        lambda: runDetachedProcess(["mullvad", "reconnect"]),
+                    )
+                ],
+            ),
+        ]
+
+    def buildItem(self, relay):
+        name = relay[0]
+        command = ["mullvad", "relay", "set", "hostname", name]
+        subtext = relay[1]
+        return StandardItem(
+            id=f"vpn-{name}",
+            text=name,
+            subtext=subtext,
+            iconUrls=self.iconUrls,
+            actions=[
+                Action(
+                    "connect",
+                    text="Connect",
+                    callable=lambda: runDetachedProcess(command),
+                ),
+                Action("copy", "Copy to Clipboard", lambda t=name: setClipboardText(t)),
+            ],
+        )
+
+    def handleTriggerQuery(self, query):
+        if query.isValid:
+            if query.string.strip():
+                relays = self.getRelays()
+                query.add([item for item in self.actions() if query.string.lower() in item.text.lower()])
+                query.add(
+                    [
+                        self.buildItem(relay)
+                        for relay in relays
+                        if all(q in relay[0].lower() for q in query.string.lower().split())
+                    ]
+                )
+            else:
+                query.add(self.defaultItems())
