e1a634f09ca36979dcd60ffac5af5aedbfd52a74
pete <work@pete-hamlin.com>
2023-11-01 13:25:59 +0000

    feat [lidarr]: Added lidarr extension

---
+++ lidarr/__init__.py
@@ -0,0 +1,291 @@
+"""Extension to interact with a Lidarr instance API.
+
+Extension supports searching existing library of artists and adding new artists.
+"""
+
+import os
+from pathlib import Path
+from time import sleep
+from typing import Dict, List
+from urllib import parse
+
+import requests
+from albert import *
+
+md_iid = "2.1"
+md_version = "2.0"
+md_name = "Lidarr"
+md_description = "Manage TV series via a Sonarr instance"
+md_license = "MIT"
+md_url = "https://github.com/Pete-Hamlin/albert-python"
+md_maintainers = ["@Pete-Hamlin"]
+md_lib_dependencies = ["requests"]
+
+
+class Plugin(PluginInstance, TriggerQueryHandler):
+    iconUrls = [f"file:{Path(__file__).parent}/lidarr.png"]
+    user_agent = "org.albert.lidarr"
+
+    def __init__(self):
+        TriggerQueryHandler.__init__(
+            self,
+            id=md_id,
+            name=md_name,
+            description=md_description,
+            synopsis="<artist>",
+            defaultTrigger="lidarr ",
+        )
+        PluginInstance.__init__(self, extensions=[self])
+
+        self._instance_url = self.readConfig("instance_url", str) or "http://localhost:8686"
+        self._api_key = self.readConfig("api_key", str) or ""
+
+        self._root_path = self.readConfig("root_path", str) or "/tv"
+        self._profile_id = self.readConfig("profile_id", int) or 1
+        self._metadata_id = self.readConfig("metadata_id", int) or 1
+        self._default_monitor = self.readConfig("default_monitor", bool) or True
+        self._delete_remove_files = self.readConfig("delete_remove_files", bool) or False
+
+        self.headers = {
+            "User_Agent": self.user_agent,
+            "X-Api-Key": self.api_key,
+            "accept": "application/json",
+        }
+
+    @property
+    def instance_url(self):
+        return self._instance_url
+
+    @instance_url.setter
+    def instance_url(self, value):
+        self._instance_url = value
+        self.writeConfig("instance_url", value)
+
+    @property
+    def api_key(self):
+        return self._api_key
+
+    @api_key.setter
+    def api_key(self, value):
+        self._api_key = value
+        self.writeConfig("api_key", value)
+
+    @property
+    def root_path(self):
+        return self._root_path
+
+    @root_path.setter
+    def root_path(self, value):
+        self._root_path = value
+        self.writeConfig("root_path", value)
+
+    @property
+    def profile_id(self):
+        return self._profile_id
+
+    @profile_id.setter
+    def profile_id(self, value):
+        self._profile_id = value
+        self.writeConfig("profile_id", value)
+
+    @property
+    def metadata_id(self):
+        return self._metadata_id
+
+    @metadata_id.setter
+    def metadata_id(self, value):
+        self._metadata_id = value
+        self.writeConfig("metadata_id", value)
+
+    @property
+    def default_monitor(self):
+        return self._default_monitor
+
+    @default_monitor.setter
+    def default_monitor(self, value):
+        self._default_monitor = value
+        self.writeConfig("default_monitor", value)
+
+    @property
+    def delete_remove_files(self):
+        return self._delete_remove_files
+
+    @delete_remove_files.setter
+    def delete_remove_files(self, value):
+        self._delete_remove_files = value
+        self.writeConfig("delete_remove_files", value)
+
+    def configWidget(self):
+        return [
+            {"type": "lineedit", "property": "instance_url", "label": "URL"},
+            {
+                "type": "lineedit",
+                "property": "api_key",
+                "label": "API key",
+                "widget_properties": {"echoMode": "Password"},
+            },
+            {"type": "lineedit", "property": "root_path", "label": "Root Path"},
+            {"type": "spinbox", "property": "profile_id", "label": "Profile ID"},
+            {"type": "spinbox", "property": "metadata_id", "label": "Metadata ID"},
+            {"type": "checkbox", "property": "default_monitor", "label": "Monitor by default"},
+            {"type": "checkbox", "property": "delete_remove_files", "label": "Delete removes files"},
+        ]
+
+    def handleTriggerQuery(self, query):
+        stripped = query.string.strip()
+        if stripped:
+            # avoid spamming server
+            for _ in range(50):
+                sleep(0.01)
+                if not query.isValid:
+                    return
+
+            if stripped.startswith("add"):
+                # Add new artist
+                if stripped[3:]:
+                    data = self.artist_lookup(stripped[3:])
+                    items = [item for item in self.gen_add_items(data)]
+                    query.add(items)
+                else:
+                    query.add(
+                        StandardItem(
+                            id=md_id, text=md_name, subtext="Add a new artist on Lidarr", iconUrls=self.iconUrls
+                        )
+                    )
+            else:
+                # Search existing artists
+                data = (item for item in self.refresh_artist() if stripped in item["artistName"].lower())
+                items = [item for item in self.gen_search_items(data)]
+                query.add(items)
+        else:
+            query.add(
+                StandardItem(
+                    id=md_id, text=md_name, subtext="Search for an existing artist on Lidarr", iconUrls=self.iconUrls
+                )
+            )
+
+    def gen_add_items(self, data: list[dict]) -> List[Item]:
+        for artist in data:
+            title = artist["artistName"]
+            subtext = "{} - {}".format(artist.get("artistType"), artist.get("status").capitalize())
+            actions = [
+                Action(
+                    "monitor",
+                    "Monitor",
+                    lambda chosen_artist=artist: self.add_artist(chosen_artist),
+                ),
+                Action(
+                    "monitor-search",
+                    "Monitor + Search",
+                    lambda chosen_artist=artist: self.add_artist(chosen_artist, search_missing=True),
+                ),
+                Action(
+                    "open",
+                    "View on Lidarr",
+                    lambda url=f"{self._instance_url}/add/new?term={title}": openUrl(url),
+                ),
+            ]
+            for link in artist["links"]:
+                if link["name"] == "musicmoz":
+                    actions.append(
+                        Action(
+                            "musicmoz",
+                            "View on MusicMoz",
+                            lambda url=link["url"]: openUrl(url),
+                        ),
+                    )
+                if link["name"] == "discogs":
+                    actions.append(
+                        Action(
+                            "discogs",
+                            "View on Discogs",
+                            lambda url=link["url"]: openUrl(url),
+                        ),
+                    )
+            yield StandardItem(id=md_id, iconUrls=self.iconUrls, text=title, subtext=subtext, actions=actions)
+
+    def gen_search_items(self, data: list[dict]) -> List[Item]:
+        for artist in data:
+            title = artist["artistName"]
+            artist_url = "{}/api/v1/artist/{}".format(self._instance_url, artist["id"])
+            albums = artist["statistics"]["albumCount"]
+            tracks = artist["statistics"]["trackFileCount"]
+            total_tracks = artist["statistics"]["trackCount"]
+            missing = total_tracks - tracks
+            subtext = f"{albums} Albums: {tracks} Tracks"
+            if missing:
+                subtext += f" - {missing} Missing"
+            yield StandardItem(
+                id="artist-{}".format(artist["id"]),
+                iconUrls=self.iconUrls,
+                text=title,
+                subtext=subtext,
+                actions=[
+                    Action(
+                        "open",
+                        "Open Artist in Lidarr",
+                        lambda open_url=artist_url: openUrl(open_url),
+                    ),
+                    Action(
+                        "rescan",
+                        "Rescan Artist",
+                        lambda artist_id=artist["id"]: self.rescan_artist(artist_id),
+                    ),
+                    Action(
+                        "delete",
+                        "Delete Artist",
+                        lambda artist_id=artist["id"]: self.delete_artist(artist_id),
+                    ),
+                ],
+            )
+
+    def artist_lookup(self, query_string: str) -> List[Dict]:
+        params = {"term": query_string.strip()}
+        url = f"{self._instance_url}/api/v1/artist/lookup/?{parse.urlencode(params)}"
+        debug(f"Making GET request to {url}")
+        response = requests.get(url, headers=self.headers)
+        if response.ok:
+            print(response.json())
+            return (artist for artist in response.json())
+        warning(f"Got response {response.status_code} when attempting to fetch artist data")
+        return []
+
+    def refresh_artist(self):
+        url = f"{self._instance_url}/api/v1/artist"
+        response = requests.get(url, headers=self.headers)
+        if response.ok:
+            return (series for series in response.json())
+        else:
+            warning(f"Got response {response.status_code} when attempting to fetch artist data")
+            return []
+
+    def add_artist(self, artist: Dict, search_missing: bool = False) -> None:
+        url = f"{self._instance_url}/api/v1/artist"
+        data = {
+            "artistName": artist["artistName"],
+            "artistType": artist["artistType"],
+            "foreignArtistId": artist["foreignArtistId"],
+            "qualityProfileId": self._profile_id,
+            "metadataProfileId": self._metadata_id,
+            "images": artist["images"],
+            "links": artist["links"],
+            "rootFolderPath": self._root_path,
+            "addOptions": {
+                "monitored": self._default_monitor,
+                "ignoreEpisodesWithoutFiles": not self._default_monitor,
+                "searchForMissingAlbums": search_missing,
+            },
+        }
+        debug(f"Sending data: {data}")
+        response = requests.post(url=url, json=data, headers=self.headers)
+        debug(f"Got response {response.status_code} from Lidarr")
+
+    def rescan_artist(self, artist_id: str) -> None:
+        url = f"{self._instance_url}/api/v1/command/"
+        data = {"name": "RescanArtist", "artistId": artist_id}
+        requests.post(url, json=data, headers=self.headers)
+
+    def delete_artist(self, artist_id: str) -> None:
+        url = f"{self._instance_url}/api/v1/artist/{artist_id}"
+        data = {"id": artist_id, "deleteFiles": self._delete_remove_files}
+        requests.delete(url, json=data, headers=self.headers)
